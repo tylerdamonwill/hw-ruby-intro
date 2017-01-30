39f9e45f8e65dcd12b96640a9ec877d44e38c7ca
tylerdamonwilliams <williams13@grinnell.edu>
2017-01-29 22:44:51 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,78 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr) arr.reduce(&:+) end
+
+def max_2_sum(arr)
+  if (arr.length == 0)
+    0
+  end
+  if (arr.length == 1)
+    arr[0]
+  end
+    arr.sort!.reverse!
+    arr[0] + arr[1]
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def sum_to_n?(vals,val)
+  vals.combination(2).any? do |x,y|
+    if (val == x+y) 
+      return true
+    end
+    return false
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
 
 # Part 2
-
 def hello(name)
-  # YOUR CODE HERE
+puts "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+s.downcase!
+test = s[0]
+  if (test == "a" ) || (test == "e") || (test == "i") || (test == "o") || (test == "u")
+    return false
+  end
+  return true
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+test = s
+  if (test.delete('01') != '')
+    return false
+  end
+  if (s.to_i(2) % 4 != 0)
+    return false
+  end
+  return true
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if (isbn.empty? = true)
+      raise ArgumentError.new("ISBN must be a string")
+  end
+    if (price <= 0)
+      raise ArgumentError.new("Price must be above 0")
+    @isbn = isbn
+    @price = price
+  end
+  def isbn
+    @isbn
+  end
+  def isbn=(new_isbn)
+    @isbn = new_isbn
+  end
+  def price
+    @price
+  end
+  def price=(new_price)
+    @price = new_price
+  end
+    def price_as_string
+      sprintf("$%2.2f", @BookInStore.price)
 end
